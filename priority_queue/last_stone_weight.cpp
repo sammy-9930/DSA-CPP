66791db7d05c67cc380d1d76d5cfd6d640ac3500
sammy-9930 <samanthamenezes99@gmail.com>
2025-08-11 08:05:10 -0400

    Last stone weight - brute force

--- priority_queue/last_stone_weight.cpp
+++ priority_queue/last_stone_weight.cpp
@@ -1,3 +1,22 @@
+// Brute force - sorting 
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        while (stones.size() > 1){
+            sort(stones.begin(), stones.end());
+            int high = stones.back();
+            stones.pop_back();
+            int sec_high = stones.back();
+            stones.pop_back();
+            if (high > sec_high)
+                stones.push_back(high - sec_high);
+        }
+        return stones.empty() ? 0 : stones[0];
+    }
+};
+
+
+// optimize - priority queue 
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
