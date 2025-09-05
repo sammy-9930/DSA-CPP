4fb1a092ecbacf5d31e4cdd414a0723464a1f55e
sammy-9930 <samanthamenezes99@gmail.com>
2025-09-05 13:20:59 -0400

    Leetcode problems

--- priority_queue/k_closest_points_to_origin.cpp
+++ priority_queue/k_closest_points_to_origin.cpp
@@ -6,18 +6,45 @@ class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
         priority_queue<pair<int, pair<int, int>>> maxHeap;
-        for(auto& point : points){
-            int dist = point[0] * point[0] + point[1] * point[1];
-            maxHeap.push({dist, {point[0], point[1]}});
+        for (auto& p : points){
+            int x = p[0], y = p[1];
+            int dist = (x*x) + (y*y);
+            maxHeap.push({dist, {x, y}});
             if (maxHeap.size() > k){
                 maxHeap.pop();
             }
         }
         vector<vector<int>> res;
-        while (!maxHeap.empty()){
-            res.push_back({maxHeap.top().second.first,
-                           maxHeap.top().second.second});
+        while(!maxHeap.empty()){
+            auto [dist, coor] = maxHeap.top();
+            res.push_back({coor.first, coor.second});
             maxHeap.pop();
+        }
+        return res;
+    }
+};
+
+//Min-heap 
+// time: O(nlogn)
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<
+            pair<int, pair<int, int>>,
+            vector<pair<int, pair<int, int>>>,
+            greater<pair<int, pair<int, int>>>
+            > minHeap;
+        for (auto& p : points){
+            int x = p[0], y = p[1];
+            int dist = (x*x) + (y*y);
+            minHeap.push({dist, {x, y}});
+        }
+        vector<vector<int>> res;
+        while (k > 0){
+            auto [dist, coor] = minHeap.top();
+            res.push_back({coor.first, coor.second});
+            minHeap.pop();
+            k --;
         }
         return res;
     }
