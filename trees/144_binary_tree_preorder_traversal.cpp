3c61f148cdfcff0bac2d8801b00f2484b40fdb23
Samantha Menezes <80445152+sammy-9930@users.noreply.github.com>
2026-05-09 17:34:52 -0400

    Add iterative solution for preorder traversal
    Implemented iterative preorder traversal for binary tree.

--- trees/144_binary_tree_preorder_traversal.cpp
+++ trees/144_binary_tree_preorder_traversal.cpp
@@ -32,7 +32,33 @@ public:
 
 
 /*Solution 2: iterative */
-class Solution{
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
 public:
-
-}
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> res;
+        if (root == NULL) return res;
+        stack<TreeNode*> stk;
+        stk.push(root);
+        while(!stk.empty()){
+            TreeNode* frontNode = stk.top();
+            res.push_back(frontNode->val);
+            stk.pop();
+            if (frontNode->right)
+                stk.push(frontNode->right);
+            if (frontNode->left)
+                stk.push(frontNode->left);
+        }
+        return res;
+    }
+};
