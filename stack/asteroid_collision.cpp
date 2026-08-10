778e2c73b27ace13b23fae3979880d39c80bbf77
Samantha Menezes <80445152+sammy-9930@users.noreply.github.com>
2026-08-10 07:40:56 -0400

    735. Asteroid Collision
    Refactored asteroidCollision function to use a two-pointer approach without a stack, improving space complexity.

--- stack/asteroid_collision.cpp
+++ stack/asteroid_collision.cpp
@@ -29,3 +29,36 @@ public:
         return res;
     }
 };
+
+/*
+Without stack
+time complexity: O(n)
+space complexity: O(1)
+*/
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        int j = -1;
+        for(int& a: asteroids){
+            while (j >= 0 && a < 0 && asteroids[j] > 0){
+                int diff = a + asteroids[j];
+                if (diff == 0){
+                    a = 0;
+                    j--;
+                    break;
+                }
+                else if (diff > 0){
+                    a = 0;
+                    break;
+                }
+                else{
+                    j--;
+                }
+            }
+            if (a!= 0)
+                asteroids[++j] = a;
+        }
+        asteroids.resize(j+1);
+        return asteroids;
+    }
+};
